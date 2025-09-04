7038e2446af67b3a7515fb7bc9dc1705e8eadb22
Manoel <manoel_jonathan@hotmail.com>
2025-09-04 11:59:34 -0300

    feat(ArvoreBinaria): adicionando árvore com generics

---
+++ 20250825-comparable/Automovel.java
@@ -0,0 +1,17 @@
+public class Automovel {
+    String anoFabricacao;
+    String anoModelo;
+    int numeroChassi;
+    String placa;
+    String cor;
+    String marca;
+    String modelo;
+    String tipo;
+  
+
+    public Automovel(String marca, String modelo, String tipo, String numeroChassi, String placa, String cor)
+    {
+        this.cor = cor;
+        this.placa = placa;
+    }
+}
